7ce6b1f1b96ccc9912656e07b6188dd74dbbf82b
Hiago Macêdo <hiago.m.a.macedo@gmail.com>
2024-03-13 12:09:52 -0300

    Fix nas operacoes com not(B)

--- ULA.sv
+++ ULA.sv
@@ -19,13 +19,14 @@ module ULA (input CLOCK_50,
 	logic [6:0] o_resultado;
 	logic o_overflow;
 	logic o_zero;
-	
+
 	always_ff @ (posedge CLOCK_50)
 	begin
+		
 		A <= SW[5:0];
 		B <= SW[11:6];
 		operacao <= SW[14:12];
-		modo <= SW[15];
+		modo <= SW[16];
 		reset <= SW[17];
 		LEDR[5:0] <= o_resultado[5:0];
 		LEDR[7] <= o_zero;
@@ -56,16 +57,40 @@ module ULA (input CLOCK_50,
 			else /* operações aritméticas */
 			begin
 				case(operacao)
-					3'b000: o_resultado <= A + B;
-					3'b001: o_resultado <= A - B;
-					3'b010: o_resultado <= A + (~B);
-					3'b011: o_resultado <= A - (~B);
-					3'b100: o_resultado <= A + 1'b1;
-					3'b101: o_resultado <= A - 1'b1;
-					3'b110: o_resultado <= B + 1'b1;
-					3'b111: o_resultado <= B - 1'b1;
+					3'b000: begin
+						o_resultado <= A + B;
+						o_overflow <= o_resultado[6];
+					end
+					3'b001: begin
+						o_resultado <= A - B;
+						o_overflow <= o_resultado[6];
+					end
+					3'b010: begin
+						o_resultado <= A + (~B);
+						o_overflow <= ~o_resultado[6];
+					end
+					3'b011: begin
+						o_resultado <= A - (~B);
+						o_overflow <= ~o_resultado[6];
+					end
+					3'b100: begin
+						o_resultado <= A + 1'b1;
+						o_overflow <= o_resultado[6];
+					end
+					3'b101: begin
+						o_resultado <= A - 1'b1;
+						o_overflow <= o_resultado[6];
+					end
+					3'b110: begin
+						o_resultado <= B + 1'b1;
+						o_overflow <= o_resultado[6];
+					end
+					3'b111: begin
+						o_resultado <= B - 1'b1;
+						o_overflow <= o_resultado[6];
+					end
 				endcase
-				o_overflow <= o_resultado[6];
+				// o_overflow <= o_resultado[6];
 			end
 			o_zero <= (o_resultado[5:0] == 6'b0);
 		end
